fd3d48050c7ae927fbf44311641df72ad509e7b4
WalnutsAreCool <claadam@nuevaschool.org>
2016-10-04 09:31:56 -0700

    Classadoo autocommit

--- pokemon.js
+++ pokemon.js
@@ -11,13 +11,13 @@ function getPokemon() {
   console.log("getting")
   $.ajax({
     method: "GET",
-    url: "https://pokeapi.co/api/v2/pokemon/666",    
+    url: "https://pokeapi.co/api/v2/pokemon/100",    
     success: function(res) {
       console.log("got it")
       console.log(res)
       Name.html(res["name"] + ":")
       ID.html("ID: " + res["id"])
-      Type.html("Type(s): " +res["types"][0].type.name)      
+      Type.html("Type(s): " +res["types"][0].type.name + ", " + res["types"][1].type.name)      
       ability.html ("Ability(ies): " + res["ability"])
     }
   })
